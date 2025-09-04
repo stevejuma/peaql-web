16ee3e8081944778dd3d6454f853f69438a22030
Steve Juma <steve@ju.ma>
2025-09-04 09:42:25 +0100

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/peaql-web",
   plugins: [tailwindcss(), svelte()],
   assetsInclude: ["**/*.sql"],
   server: {
